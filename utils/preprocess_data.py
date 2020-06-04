2806e4892130d4f1659bd075cd6908016275b688
trungmanhhuynh <trungmanhhuynh@gmail.com>
2020-06-03 20:59:43 -0600

    add utils

---
+++ utils/preprocess_data.py
@@ -0,0 +1,36 @@
+'''
+
+Author: Manh Huynh
+Last Update: 06/03/2020
+'''
+
+JAAD_DATASET_PATH = "./datasets/JAAD"
+
+
+
+
+if __name__ == '__main__':
+
+
+'''
+Pre-requisites:
+   Make sure JAAD dataset available in JAAD_DATASET_PATH. This can be done by
+   running the following steps
+   >> mkdir datasets
+   >> git clone https://github.com/ykotseruba/JAAD.git 
+   >> cd JAAD 
+   >> sh download_clips.sh                 # download the JAAD clips 
+   >> sh split_clips_to_frames.sh          # parse clips into images 
+'''
+
+
+# 1. Read ground truth pedestrian information given by JAAD dataset. 
+
+
+
+# 2. Read Pose data 
+
+
+# 3. Merge pedestrians'id and pose 
+
+ 
