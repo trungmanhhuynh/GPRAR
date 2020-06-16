b869ec4841b3ae4d11116af4951140c90e1cd3ef
trungmanhhuynh <trungmanhhuynh@gmail.com>
2020-06-15 19:48:24 -0600

    done extract raw pose feature

--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -4,7 +4,49 @@ Author: Manh Huynh
 Last Update: 06/03/2020
 '''
 
-JAAD_DATASET_PATH = "./datasets/JAAD"
+import sys
+import os
+
+#from datasets.JAAD.jaad_data import JAAD
+#sys.path.append('..')
+
+
+JAAD_DATASET_DIR = "/home/manhh/github/datasets/JAAD"
+PROCESSED_DATA_DIR = "/home/manhh/github/Traj-STGCNN/processed_data/JAAD"
+OPENPOSE_DIR =  "/home/manhh/github/openpose/build/examples/openpose"
+
+
+def generate_pose_data():
+
+	#./build/examples/openpose/openpose.bin --image_dir ~/github/datasets/eth_mobile/images/eth_bahnhof/--write_json ./mytest/ --display 0 --render_pose 0 
+
+
+	images_dir = os.path.join(JAAD_DATASET_DIR, "images") 
+	for video_name in os.listdir(images_dir): 
+
+		print("Generate pose data for : ", video_name)
+
+		processed_pose_dir = os.path.join(PROCESSED_DATA_DIR, "pose", video_name)
+		if not os.path.exists(processed_pose_dir):
+			os.makedirs(processed_pose_dir)
+
+		cmd = "cd /home/manhh/github/openpose && {0} --image_dir {1} --write_json {2} --display 0 --render_pose 0".format(
+			os.path.join(OPENPOSE_DIR, "openpose.bin"), 
+			os.path.join(images_dir, video_name), 
+			processed_pose_dir) 
+
+		os.system(cmd)
+
+
+def generate_location_data():
+
+
+	 # 1. Read ground truth pedestrian information given by JAAD dataset. 
+	imdb = JAAD(data_path=JAAD_DATASET_PATH)
+	jaad_data = imdb.generate_database()
+
+	print(jaad_data)
+	input("jere")
 
 
 
@@ -12,25 +54,5 @@ JAAD_DATASET_PATH = "./datasets/JAAD"
 if __name__ == '__main__':
 
 
-'''
-Pre-requisites:
-   Make sure JAAD dataset available in JAAD_DATASET_PATH. This can be done by
-   running the following steps
-   >> mkdir datasets
-   >> git clone https://github.com/ykotseruba/JAAD.git 
-   >> cd JAAD 
-   >> sh download_clips.sh                 # download the JAAD clips 
-   >> sh split_clips_to_frames.sh          # parse clips into images 
-'''
 
-
-# 1. Read ground truth pedestrian information given by JAAD dataset. 
-
-
-
-# 2. Read Pose data 
-
-
-# 3. Merge pedestrians'id and pose 
-
- 
+	generate_pose_data() 
